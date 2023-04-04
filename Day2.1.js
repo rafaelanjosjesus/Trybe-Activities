de66df6e160f7b71ca4c4d19eefc6d4bfbaf3b93
rafaelanjosjesus <veigarnodraiven@gmail.com>
2023-04-04 13:23:06 -0300

    I finish this activity

---
+++ Day2.1.js
@@ -0,0 +1,9 @@
+let menu = ['Home', 'Services', 'Portfolio', 'Links'];
+menu.push("contact");
+console.log(menu[4]);
+
+let menuServices = menu [2];
+console.log(menuServices);
+
+let indexOfPortfolio = menu [3];
+console.log(indexOfPortfolio);
