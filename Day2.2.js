0d631403ffd0d361047fae24b6720a1a1572d808
rafaelanjosjesus <veigarnodraiven@gmail.com>
2023-04-04 15:50:36 -0300

    I finish my activity

---
+++ Day2.2.js
@@ -0,0 +1,4 @@
+let groceryList = ["Egg", "Milk", "Meat", "Avocado", "Apple"];
+    for (let indice = 0; indice < groceryList.length; indice += 1){
+        console.log("I need one " + groceryList[indice]);
+    }
