042bda9a01d927d82f3a7d3fad7f5ddf677aeb27
rafaelanjosjesus <veigarnodraiven@gmail.com>
2023-04-03 21:51:46 -0300

    I finish activity

---
+++ Day1.5.js
@@ -0,0 +1,6 @@
+let weekDay = "Wednesday"
+if(weekDay === "Monday" || weekDay === "Tuesday" || weekDay === "Wednesday" || weekDay === "Thursday" || weekDay === "Friday"){
+    console.log("Yay, another day of learning at Trybe >:D");
+} else {
+    console.log("FINALLY, well deserved rest! UwU!")
+}
