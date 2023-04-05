9dee17738cf9be026b2768237e9c9be426deb412
rafaelanjosjesus <veigarnodraiven@gmail.com>
2023-04-05 15:42:50 -0300

    I finish my first activity

---
+++ Day2.6.js
@@ -0,0 +1,6 @@
+//Percorra o array imprimindo todos os valores contidos nele com a função console.log(). -- Iterate through the array by printing all the values ​​contained in it with the console.log().
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+    for (let index = 0; index < numbers.length; index += 1) {
+        console.log(numbers[index]);
+    }
