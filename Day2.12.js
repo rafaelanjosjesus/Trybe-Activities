c889b27751587930eab1b6ff26dab344b2d0ebd4
rafaelanjosjesus <veigarnodraiven@gmail.com>
2023-04-06 15:36:08 -0300

    I finish my activity

---
+++ Day2.12.js
@@ -0,0 +1,7 @@
+//Utilizando for, crie um array que vá de 1 a 25 e imprima o resultado. // Using for, create an array that goes from 1 to 25 and print the result.
+
+let numbers = []
+for (let index = 1; index <= 25; index += 1){
+    numbers.push(index);
+}
+console.log(numbers);
